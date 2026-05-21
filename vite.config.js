6ae0a24a5fb9595bfb9c2b1ef08dfc1663c5d4ef
kutya001 <111129686+kutya001@users.noreply.github.com>
2026-05-21 13:11:06 +0600

    4

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: './',
+  base: '/Shaiba/',
   plugins: [
     vue(),
     tailwindcss()
